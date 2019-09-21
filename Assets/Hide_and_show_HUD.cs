6b4c380caeac4c8baf4c4e123542edd5f4573e12
Carl Summers <summers.carl@gmail.com>
2019-09-21 14:58:15 -0400

    Add HUD button and enable disable display

---
+++ Assets/Hide_and_show_HUD.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hide_and_show_HUD : MonoBehaviour
+{
+	public GameObject generalHUD;
+	public GameObject tempHUD;
+	public GameObject Humidity;
+	public GameObject pressure;
+	public GameObject PM;
+	public GameObject UV;
+	public GameObject buttonToActivate;
+	bool isHudShown = false;
+	bool isClicked = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+		tempHUD.SetActive(isHudShown);
+		Humidity.SetActive(isHudShown);
+		PM.SetActive(isHudShown);
+		UV.SetActive(isHudShown);
+		pressure.SetActive(isHudShown);
+		buttonToActivate.SetActive(true);
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+		
+		if (Input.GetKey(KeyCode.Space))
+		{
+			tempHUD.SetActive(!isHudShown);
+			Humidity.SetActive(!isHudShown);
+			PM.SetActive(!isHudShown);
+			UV.SetActive(!isHudShown);
+			pressure.SetActive(!isHudShown);
+			buttonToActivate.SetActive(false);
+		}
+
+		if(Input.GetKey(KeyCode.C) )
+		{
+			tempHUD.SetActive(false);
+			Humidity.SetActive(false);
+			PM.SetActive(false);
+			UV.SetActive(false);
+			pressure.SetActive(false);
+			buttonToActivate.SetActive(true);
+		}
+		
+	}
+
+	public void displayHUD()
+	{
+		tempHUD.SetActive(!isClicked);
+		Humidity.SetActive(!isClicked);
+		PM.SetActive(!isClicked);
+		UV.SetActive(!isClicked);
+		pressure.SetActive(!isClicked);
+		buttonToActivate.SetActive(false);
+	}
+
+	
+}
